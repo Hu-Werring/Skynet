9a5da69ac00767be699923442b12638d4a2839d2
Werring <thomwerring@gmail.com>
2011-06-14 08:21:41 +0200

    Added docblocks

--- classes/core/registery.php
+++ classes/core/registery.php
@@ -6,10 +6,29 @@
  */
 
 class Core_Registery {
+    
+    /**
+     * $instance
+     * stores link to itself for singleton instance
+     * @access private
+     * @staticvar Object singleton holder 
+    */
     private static $instance;
     
+    /**
+     * $classes
+     * Stores links to all other classes
+     * @access private
+     * @var Array links to other classes
+    */
     private $classes = null;
     
+    /**
+     * singleton
+     * Creates and/or returns instance of itself
+     * @static
+     * @return Object The current Core_Registery object
+    */
     static function singleton(){
         if(is_null(self::$instance)){
             $c = __CLASS__;
@@ -19,28 +38,38 @@ class Core_Registery {
         return self::$instance;
     }
     
+    /**
+     * __construct
+     * initiate registery
+    */
     function __construct(){
         if(!is_null(self::$instance)){
             die("Use " . __CLASS__ . "::singleton() instead of new " . __CLASS__ . "()");
         } else {
             self::$instance = $this;
         }
-        $this->init();
     }
     
-    private function init(){
-        
-    }
-    
-    function __set($name, $value){
+    /**
+     * __set
+     * magic function to created links to new classes
+     * @param String $name Name of the class
+     * @param Object $value Link to the object
+    */
+    public function __set($name, $value){
         $this->classes[$name] = $value;
     }
     
-    function __get($name){
+    /**
+     * __get
+     * magic function to call object stored in registery
+     * @param String $name Name of the class
+     * @return Object Link to the object
+    */
+    public function __get($name){
         if(isset($this->classes[$name]))
             return $this->classes[$name];
         else
             return null;
     }
-    
 }
