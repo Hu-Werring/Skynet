1475dba171d179463531e7163b0010cb26c26c07
Werring <thomwerring@gmail.com>
2011-06-14 08:04:27 +0200

    Created temp  config, complete config class is for lucas

---
+++ classes/core/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * class Core_Config
+ */
+
+class Core_Config  {
+    
+    private $reg;
+    
+    private $storage;
+    
+    function __construct() {
+        $this->reg = Core_Registery::singleton();
+        $this->reg->config = $this;
+        $this->storage["db_host"] = "localhost";
+        $this->storage["db_user"] = "project4";
+        $this->storage["db_pass"] = "project4";
+        $this->storage["db_daba"] = "project4";
+        
+    }
+    
+    public function getvar($name){
+        if(isset($this->storage[$name])){
+            return $this->storage[$name];
+        } else {
+            return null;
+        }
+    }
+}
+
+?>
