5bacd4a529866381aba3f92f2da576125d75e985
Werring <thomwerring@gmail.com>
2011-06-15 22:42:53 +0200

    Create default tables

--- install/index.php
+++ install/index.php
@@ -14,5 +14,26 @@
     
     $skynet = new Core_Skynet();
     $skynet->install();
+    $reg = Core_Registery::singleton();
     
-?>
+    
+    
+
+    $dbStruct = file_get_contents(basedir . "install" . DS . "databaseStructure.json");
+    
+    $tables = json_decode($dbStruct,true);
+
+    $output = "";
+    foreach($tables as $table=>$fields){
+        
+        $output .= $reg->database->prefixTable($table) . ": ";
+        $succes = $reg->database->createTable($table,$fields,true);
+        $output .= var_export($succes,true) . PHP_EOL;
+        if(!$succes){
+            $output .= $reg->database->lastError();
+        }
+    
+    //echo '$tables["' . $table . '"] = ' . var_export($fields,true) . ";" . PHP_EOL . PHP_EOL;
+    }
+    /**/
+    echo nl2br($output);
