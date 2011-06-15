b25ef3014e3f3b2ebfaf0d88bf0b7ec9cb0678e8
Werring <thomwerring@gmail.com>
2011-06-15 11:57:16 +0200

    Created acp index.php

---
+++ acp/index.php
@@ -0,0 +1,17 @@
+<?php
+    define("DS",DIRECTORY_SEPARATOR);
+    define("basedir",$_SERVER['DOCUMENT_ROOT'] . DS);
+    echo basedir;
+    /**
+     * Autoloader for functions, easy include
+     * Using lazyman namespaces
+    */  
+    function __autoload($className) {
+        require_once basedir . "classes/" . strtolower(str_replace("_","/",$className)) . ".php";
+    }
+    
+    
+    //create skynet website
+    $skynet = new Core_Skynet();
+    $skynet->acp();
+
