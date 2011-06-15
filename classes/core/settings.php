696a3a1206caf20006045f68247cacccdbf6360a
Lucas Weijers <lucasweijers@MacBook-Pro-van-Lucas-Weijers.local>
2011-06-15 12:13:37 +0200

    moved debug check to debug clas. and moved init debug to core_skynet

--- classes/core/settings.php
+++ classes/core/settings.php
@@ -31,24 +31,8 @@ class Core_Settings
         $this->reg = Core_Registery::singleton();
         $this->reg->settings = $this;
         
-        /**
-         * Define base_url
-         * @global base_url
-        */
-        //define(base_url, __FILE__);
-        
-        /**
-         * Define DS
-         * Defining the directory seperator
-         */
-        define('DS', DIRECTORY_SEPARATOR);
-        
         //Deze functie eigenlijk vanaf de index.php aanroepen. Voor developing nu even zo.
-        $this->setIni('settings'. DS .'settings.ini');
-        if($this->settings['system']['modus'] === 'debug' || ($_SERVER['REMOTE_ADDR'] && $this->settings['system']['modus'] === 'localdebug'))
-        {
-            new Debug_Main();
-        }
+        $this->setIni(basedir .'settings'. DS .'settings.ini');
         $this->reg->debug->print_pre($this->settings);
     }
     
--- classes/debug/main.php
+++ classes/debug/main.php
@@ -13,8 +13,15 @@ class Debug_Main  {
      * creates debug class.
     */
     function __construct() {
-        $this->reg = Core_Registery::singleton();
-        $this->reg->debug = $this;
+        if($this->settings['system']['modus'] === 'debug' || ($_SERVER['REMOTE_ADDR'] === '127.0.0.1' && $this->settings['system']['modus'] === 'localdebug'))
+        {
+            $this->reg = Core_Registery::singleton();
+            $this->reg->debug = $this;
+        }
+        else
+        {
+            unset($this);
+        }
     }
     
     /**
