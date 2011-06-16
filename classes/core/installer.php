ee4df25046dc9c21e272a8213de270eebc3a0f41
Werring <thomwerring@gmail.com>
2011-06-16 23:07:49 +0200

    Check if tables are already in daba

--- classes/core/installer.php
+++ classes/core/installer.php
@@ -13,34 +13,56 @@ class Core_Installer {
     */
     private $reg = null;
     
-    
+    private $output;
     /*
      * __construct()
      */
     
     function __construct() {
         $this->reg = Core_Registery::singleton();
-        $this->createTables();
+        $this->checkTables();
+        //$this->createTables();
+        
+        echo nl2br($this->output);
     }
     
-    public function createTables(){
+    private function checkTables(){
+        
+        $exists = false;
+        $oldTables = $this->reg->database->query("show tables");
+        $dbStruct = base64_decode(file_get_contents(basedir . "install" . DS . "databaseStructure"));
+    
+        $newTables = json_decode($dbStruct,true);
+        foreach($newTables as $key=>$value){
+            
+            $tlist[$this->reg->database->prefixTable($key)] = true;
+        }
+        foreach($oldTables as $key=>$table){
+            if(!is_numeric($key)) continue;
+            if($tlist[$table['Tables_in_project4']] === true){
+                $exists = true;
+                break;
+            }
+        }
+        if($exists){
+            $this->output = "We have detected a possible earlier install of skynet, do you want to override these tables?";
+        }
+    }
+    private function createTables(){
         $dbStruct = base64_decode(file_get_contents(basedir . "install" . DS . "databaseStructure"));
     
         $tables = json_decode($dbStruct,true);
     
-        $output = "";
-        
-        $output="";
+        $this->output = "";
         foreach($tables as $table=>$fields){
             
-            $output .= $this->reg->database->prefixTable($table) . ": ";
+            $this->output .= $this->reg->database->prefixTable($table) . ": ";
             $succes = $this->reg->database->createTable($table,$fields,true);
-            $output .= var_export($succes,true) . PHP_EOL;
+            $this->output .= var_export($succes,true) . PHP_EOL;
             if(!$succes){
-                $output .= $this->reg->database->lastError();
+                $this->output .= $this->reg->database->lastError();
             }
         }
-        echo nl2br($output);
 }
 }
 
