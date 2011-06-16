754baf2fcba8522ca20fba262575534faeb3dac1
Werring <thomwerring@gmail.com>
2011-06-16 08:36:06 +0200

    Moved DaBa creation to own class

--- install/index.php
+++ install/index.php
@@ -14,26 +14,3 @@
     
     $skynet = new Core_Skynet();
     $skynet->install();
-    $reg = Core_Registery::singleton();
-    
-    
-    
-
-    $dbStruct = base64_decode(file_get_contents(basedir . "install" . DS . "databaseStructure"));
-    
-    $tables = json_decode($dbStruct,true);
-
-    $output = "";
-    foreach($tables as $table=>$fields){
-        
-        $output .= $reg->database->prefixTable($table) . ": ";
-        $succes = $reg->database->createTable($table,$fields,true);
-        $output .= var_export($succes,true) . PHP_EOL;
-        if(!$succes){
-            $output .= $reg->database->lastError();
-        }
-    
-    //echo '$tables["' . $table . '"] = ' . var_export($fields,true) . ";" . PHP_EOL . PHP_EOL;
-    }
-    /**/
-    echo nl2br($output);
