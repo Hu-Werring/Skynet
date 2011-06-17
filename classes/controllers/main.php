1a468303dfadbb1284a9b400e4b9d049dc82bc10
Lucas Weijers <lucasweijers@MacBook-Pro-van-Lucas-Weijers.local>
2011-06-17 16:18:00 +0200

    Created controllers

---
+++ classes/controllers/main.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * class Controllers_Main
+ */
+
+class Controllers_Main {
+    
+    /**
+     * $reg
+     * Link to registery
+     * @access private
+    */
+    private $reg = null;
+    
+    /**
+     *$view
+     *Direct link to view class. (From registry).
+     *@access private
+     */
+    private $view;
+
+    /**
+     * __construct
+     * creates base for CMS
+    */
+    function __construct() {
+        $this->reg = Core_Registery::singleton();
+        $this->reg->controller = $this;
+        $this->view = $this->reg->view;
+        
+        //Actie aanroepen. Dus: als www.skynet.nl/test dan TestAction();
+        if(isset($_GET['page']))
+        {
+            $action = strtolower($_GET['page']).'Action';
+            if(function_exists($action))
+            {
+                $this->$action.'()';
+            }
+            else
+            {
+                $this->indexAction();
+            }
+            //print_r($_GET)
+        }
+        else
+        {
+            $this->indexAction();
+        }
+    }
+    
+    private function indexAction()
+    {
+        $this->view->assign('content', 'grapje');
+        $this->view->assign('contentTpl', 'home');
+        $this->view->draw('main');
+    }
+    
+    private function testAction()
+    {
+        $this->view->assign('contentTpl', 'home');
+        $this->view->draw('main');
+    }
+    
+   
+}
+
+?>
