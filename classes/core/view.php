cb4614ee26a0e69a8238d2523d6995ae8040503e
Lucas Weijers <lucasweijers@MacBook-Pro-van-Lucas-Weijers.local>
2011-06-19 16:38:21 +0200

    Added add_css and add_js functions. You can load them into templates with includeLibs function.

--- classes/core/view.php
+++ classes/core/view.php
@@ -31,9 +31,22 @@ class Core_View
      *Link to the template engine
      *@access public
      */
-    
     public $tpl;
     
+    /**
+     *$css_files
+     *array that contains the css files.
+     *@access public
+     *@var array contains css filenames.
+     *
+     *$js_files
+     *array that contains the js files
+     *@access public
+     *@var array contains js filenames
+     */
+    public $css_files = array();
+    public $js_files  = array();
+        
     /**
      * __construct
      * Constructor, initialize class, define template var, load settings from settings.ini
@@ -101,7 +114,81 @@ class Core_View
         }
     }
 
+    /**
+     * add_css
+     * add css file to be included with the template
+     * @access public
+     * @param $filename The filename of the css file.
+     */
+    
+    public function add_css ( $filename )
+    {
+            if( in_array( $filename, $this->css_files ) == true )
+            {
+                    return false;
+            }
 
+            $path = basedir . $this->layoutPath . DS . 'css' . DS . $filename;
+
+            if( file_exists($path) == false )
+            {	
+                    //log::write("Css file not found. File: `$path`", $this, 'add_css()');
+                    return false;
+            }
+            $this->css_files[] = $filename;
+    }
+
+    /**
+     * add_js
+     * add js file to be included with the template
+     * @access public
+     * @param $filename The filename of the js file.
+     */
+    public function add_js ( $filename )
+    {
+            if( in_array( $filename, $this->js_files ) == true )
+            {
+                    return false;
+            }
+            
+            $path = basedir . $this->layoutPath . DS . 'js' . DS . $filename;
+            
+            if( file_exists($path) == false )
+            {
+                    //log::write("Js file not found. File: `$path`", $this, 'add_js()');
+                    return false;
+            }
+            
+            $this->js_files[] = $filename;
+    }
+    
+    /**
+     * includeLibs
+     * include the css and js scripts / libraries in the template.
+     * @access public
+     */
+    public function includeLibs ()
+    {
+            $string = '';
+            
+            if($this->css_files)
+            {
+                    foreach( $this->css_files as &$css_file )
+                    {
+                            $string .= PHP_EOL . '<link rel="stylesheet" href="' . $this->templatePath . '/css/' . $css_file . '" type="text/css" />';
+                    }
+            }
+            
+            if($this->js_files)
+            {
+                    foreach( $this->js_files as &$js_file )
+                    {
+                            $string .= PHP_EOL . '<script src="' . $this->templatePath . '/js/' . $js_file . '" type="text/javascript"></script>';
+                    }
+            }
+            
+            return $string . PHP_EOL;
+    }
 }
 
 ?>
