4f095b5d78ab1d1e2c1872795664a0ff3704c485
Werring <thomwerring@gmail.com>
2011-06-21 15:31:23 +0200

    managers

---
+++ classes/manager/base.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * page.php
+ * @package manager_page
+*/
+/**
+ * class Manager_Base
+ * Base class for all managers
+ * @version 0.1
+ * @author Thom Werring <info@werringweb.nl>
+ * @copyright Copyright (c) 2011, Thom Werring & Lucas Weijers
+ * @package Skynet_managers
+ */
+class Manager_Base {
+    
+    /**
+     * $reg
+     * registery holder
+    */
+    protected $reg = null;
+    /**
+     * __construct()
+     * Init page Manager
+     */
+    function __construct() {
+        $this->reg = Core_Registery::singleton();
+    }
+}
+?>
