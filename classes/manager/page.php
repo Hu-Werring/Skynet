0a7f040edec35b0b363b1fd22405624791dd0080
Werring <thomwerring@gmail.com>
2011-06-21 18:42:33 +0200

    Basic page loader

--- classes/manager/page.php
+++ classes/manager/page.php
@@ -16,10 +16,49 @@
 class Manager_Page extends Manager_Base {
         
     function __construct() {
-        $this->init();
+        $this->init("pages");
+        
     }
-    function test(){
-        var_dump($this->reg);
+    
+    function getPageContent($page){
+        
+        $pages = $this->reg->database->prefixTable("pages");
+        $templates = $this->reg->database->prefixTable("templates");
+        $pcontent = $this->reg->database->prefixTable("pagecontent");
+        $artikelen = $this->reg->database->prefixTable("artikelen");
+        
+        $query = "SELECT ".$templates.".Locatie,".$artikelen.".Titel,".$artikelen.".Content,".$artikelen.".LastUpdate FROM `".$pages."`
+                    inner join `".$pcontent."`on ".$pcontent.".pID=".$pages.".ID
+                    inner join `".$artikelen."`on ".$pcontent.".aID=".$artikelen.".ID
+                    inner join `".$templates."` on ".$this->reg->database->prefixTable("templates").".ID=".$this->reg->database->prefixTable("pages").".tID
+                    WHERE `".$pages."`.`Naam`='".$page."'";
+        $result = $this->reg->database->query($query);
+        
+        foreach($result as $key=>$content){
+            if(!is_numeric($key)) continue;
+                $templateLoc = $content['Locatie'];
+                unset($content['Locatie']);
+                $contents[] = $content;
+        }
+        
+        unset($query,$result);
+        $result = $this->reg->database->select("pages","Naam","ORDER BY `$pages`.`Positie` ASC");
+        
+        
+        foreach($result as $key=>$menuItem){
+            if(!is_numeric($key)) continue;
+            if($menuItem['Naam']===$page){
+                $menuItem['Current'] = true;
+            } else {
+                $menuItem['Current'] = false;
+            }
+            $menu[] = $menuItem;
+        }
+        $this->reg->view->assign('contentTpl', $templateLoc);
+        $this->reg->view->assign('content',$contents);
+        $this->reg->view->assign('menu',$menu);
+        $this->reg->view->draw('main');
     }
+
 }
 ?>
