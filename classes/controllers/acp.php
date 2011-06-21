e9cd82eae5775d84d202f5302f438cfaa6de222d
Lucas Weijers <lucasweijers@MacBook-Pro-van-Lucas-Weijers.local>
2011-06-21 15:43:28 +0200

    changed route function in controllers. when function step_1 doesnt exist it wil call step(1)

--- classes/controllers/acp.php
+++ classes/controllers/acp.php
@@ -43,11 +43,20 @@ class Controllers_Acp {
         {
             //remove install/ from begin of string and change / to "_"
             $action = str_replace("/","_",str_replace("acp/","",$_GET['page']));
+            $action2 = explode("_", $action);            
+            $actionArgs = $action2[(count($action2)-2)]; #-2 cause last char is also a /
+            $action2 = explode("_", $action, "-1");
+
             //substr last char since that is always a /
             $action = strtolower(substr($action,0,-1)).'Action';
+            $action2 = strtolower(substr($action,0,-1)).'Action';
             if(method_exists($this, $action))
             {
                 call_user_func(array($this,$action));
+            }
+            elseif(method_exists($this, $action2))
+            {
+                call_user_func_array(array($this,$action2), $actionArgs);
             }
             else
             {
@@ -78,11 +87,25 @@ class Controllers_Acp {
      * action triggerd when someone looks at the /user/ page
      * @access private
     */
-    private function userAction()
+    private function mngr_userAction($args=null)
     {
-        $this->view->assign('contentTpl', 'overview');
-       // $this->view->assign('content', 'HAha test');
+        $this->view->add_css('style.css');
+        $actions = array("User list", "New user", "blacklist");
+        /*
+        switch($args)
+        {
+            case 1:
+                break;
+            
+            default:
+            //
+        }
+        */
+        $this->view->assign('cmsActions', $actions);
+        $this->view->assign('contentTpl', 'user');
         $this->view->draw('main');
+        
+        
     }
     
     
