49f1771e16755d06bdb55a8cf4f0df34cea47d5e
Werring <thomwerring@gmail.com>
2011-06-22 22:23:41 +0200

    added actions for new page and overview

--- classes/controllers/acp.php
+++ classes/controllers/acp.php
@@ -157,12 +157,39 @@ class Controllers_Acp {
                     $pageList[$pages[$i]['pNaam']] = array("tID"=>$pages[$i]['tID'],"Template"=>$pages[$i]['tNaam'],"pID"=>$pages[$i]['pID'],"pNaam"=>$pages[$i]["pNaam"]);
                     
                 }
+                $this->view->assign('pageList', $pageList);
                 $cmsContent = "pageOverview";
+                break;
+            case 'new':
+                $cmsContent = 'pageNew';
+                $artikelList = $pMngr->getArtikelen();
+                $catList = $pMngr->getCats();
+                $itemList = array_merge($artikelList,$catList);
+                $tempList = $pMngr->getTemplates();
+                $csMenu = "<select name='content'>";
+                foreach($itemList as $value){
+                    $eVal = explode("|",$value);
+                    $sValue = array_pop($eVal);
+                    $sName  = implode("|",$eVal); 
+                    $csMenu .= "<option value='$sValue'>$sName</option>" . PHP_EOL;
+                }
+                $csMenu .= "</select>";
+                
+                $tsMenu = "<select name='template'>";
+                foreach($tempList as $value){
+                    $eVal = explode("|",$value);
+                    $sValue = array_pop($eVal);
+                    $sName  = implode("|",$eVal); 
+                    $tsMenu .= "<option value='$sValue'>$sName</option>" . PHP_EOL;
+                }
+                $tsMenu.= "</select>";
+                $this->view->assign("contentSelectMenu",$csMenu);
+                $this->view->assign("templateSelectMenu",$tsMenu);
+                
                 break;
         }
         
         $this->view->assign('cmsActions', $actions);
-        $this->view->assign('pageList', $pageList);
         $this->view->assign('contentTpl', $cmsContent);
         $this->view->draw('main');
     }
